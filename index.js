29baaca77532218fe10c5aa49a9aa95cb13423f4
zavx0z <zavx0z@yahoo.com>
2023-11-22 17:24:58 +0300

    load lib

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ class Component extends HTMLElement {
   input = {}
   output = {}
   property = {}
-  static observedAttributes = ["mobile"]
+  static observedAttributes = []
   constructor() {
     super()
     const shadow = this.attachShadow({ mode: "closed" })
-    shadow.innerHTML = html`<h1>skeleton-tracing</h1>`
+    shadow.innerHTML = html`
+      <script src="https://cdn.jsdelivr.net/npm/skeleton-tracing-wasm/build/trace_skeleton_wasm.js"></script>
+      <h1>skeleton-tracing</h1>
+    `
+    console.log("constructor")
+    // console.log(TraceSkeleton)
+    // TraceSkeleton.load().then((tracer) => {
+    //   // wasm module loaded
+    //   // here is your code
+    //   console.log(tracer)
+    //   // const { polylines,rects } = tracer.fromCanvas(HTMLCANVAS)
+    // })
   }
   render() {}
   connectedCallback() {}
