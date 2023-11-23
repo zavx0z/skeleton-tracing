9c2b1a32b2e4959c7b07cc994571138cc15ac50c
zavx0z <metaversebdfl@gmail.com>
2023-11-23 23:35:00 +0300

    props attrs

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 // https://github.com/LingDong-/skeleton-tracing/tree/master/wasm
 const html = String.raw
 class Component extends HTMLElement {
-  name = "skeleton-tracing"
+  static tag = "skeleton-tracing"
+  name = { ru: "Центральная линия", en: "skeleton-tracing" }
+  description = { ru: "", en: "" }
   input = {
     img: {
       name: { ru: "Бинарное изображение" },
       description: { ru: "" },
       type: "String",
+      variant: "Path",
       value: "",
     },
   }
   output = {
-    img: {
+    polylines: {
+      name: { ru: "Массив точек линий" },
+      description: { ru: "" },
+      type: "Array.Array.Number",
+      variant: "JS",
+      value: [],
+    },
+    svg: {
       name: { ru: "Центральная линия" },
       description: { ru: "Векторизованное изображение" },
       type: "String",
+      variant: "SVG",
       value: "",
     },
   }
-  property = {}
-  static observedAttributes = ['src']
+  property = {
+    preview: {
+      name: { ru: "Предпросмотр" },
+      description: { ru: "" },
+      type: "Boolean",
+      value: true,
+    },
+  }
+  static observedAttributes = ["src", "preview"]
   constructor() {
     super()
     this.shadow = this.attachShadow({ mode: "closed" })
+    setTimeout(() => {
+      console.log(this.property.preview.value, this.hasAttribute("preview"), typeof this.getAttribute("preview"), this.getAttribute("preview"))
+    }, 1000)
   }
   connectedCallback() {
-    this.render()
+    this.render("./bin.png")
   }
   attributeChangedCallback(attrName, oldValue, newValue) {
-    console.log(attrName)
+    switch (attrName) {
+      case "preview":
+        this.property.preview.value = this.hasAttribute("preview")
+        break
+      default:
+        break
+    }
+    setTimeout(() => {
+      console.log(attrName, oldValue, typeof newValue, this.hasAttribute("preview"))
+    }, 1000)
   }
-  render() {
+  render(src) {
     import("https://cdn.jsdelivr.net/npm/skeleton-tracing-wasm/build/trace_skeleton_wasm.min.js").then(() => {
       TraceSkeleton.load().then((tracer) => {
         const canvas = document.createElement("canvas")
@@ -41,17 +71,19 @@ class Component extends HTMLElement {
           canvas.height = img.height
           ctx.drawImage(img, 0, 0)
           const result = tracer.fromCanvas(canvas)
-
           const viz = tracer.visualize(result)
-          this.shadow.innerHTML = viz
+          setTimeout(()=>console.log(this.property.preview.value), 1000)
+          if (this.hasAttribute("preview")) this.shadow.innerHTML = viz
           canvas.remove()
+          this.subscriptions.forEach((cb) => cb({ polylines: result.polylines, svg: viz }))
         }
-        img.src = "./bin.png"
+        img.src = src
       })
     })
   }
-  send(message) {
-    this.render()
+  mailbox = [] 
+  send({ src }) {
+    this.render(src)
   }
   subscriptions = []
   subscribe(cb) {
@@ -59,4 +91,4 @@ class Component extends HTMLElement {
     return () => this.subscriptions.unshift(cb)
   }
 }
-customElements.define("skeleton-tracing", Component)
+customElements.define(Component.tag, Component)
