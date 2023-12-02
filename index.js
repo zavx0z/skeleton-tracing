38784af8733ea3c4b88d50f82ce83fb3304e3a44
zavx0z <metaversebdfl@gmail.com>
2023-12-02 19:37:14 +0300

    restruct

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 // https://github.com/LingDong-/skeleton-tracing/tree/master/wasm
+// https://zavx0z.github.io/everywhere-everything/where/{id}.xml
 import { visualize } from "./lib/draw.js"
 
 class Component extends HTMLElement {
   static tag = "skeleton-tracing"
-  name = { ru: "Центральная линия", en: "skeleton-tracing" }
-  description = { ru: "", en: "" }
+  static name = { ru: "Центральная линия", en: "skeleton-tracing" }
+  static description = { ru: "", en: "" }
   input = {
-    1701380269254: "", //"Бинарное изображение"
+    binaryImage: {
+      name: { ru: "Бинарное изображение" },
+      port: 201701380269254,
+      value: "",
+    },
   }
   output = {
-    1701435712847: [[]], // "Массив точек линий"
-    1701435696745: "", //"Центральная линия"
+    pointArray: {
+      port: 1701435712847,
+      value: [[]],
+    },
+    centerLine: {
+      port: 1701435696745,
+      value: "",
+    },
   }
   property = {
-    preview: {
-      label: { ru: "Предпросмотр" },
-      description: { ru: "" },
-      type: "Boolean",
-      value: true,
-    },
+    debug: true,
   }
   constructor() {
     super()
     this.shadow = this.attachShadow({ mode: "closed" })
   }
-  attributeChangedCallback(attrName, oldValue, newValue) {}
-  connectedCallback() {}
+  send(data) {
+    this.property.debug = true
+    console.log(data)
+    // this.render({ imageData })
+  }
+  subscriptions = []
+  subscribe(cb) {
+    this.subscriptions.push(cb)
+    return () => this.subscriptions.unshift(cb)
+  }
   render({ imageData }) {
     import("https://cdn.jsdelivr.net/npm/skeleton-tracing-wasm/build/trace_skeleton_wasm.min.js").then(() => {
       TraceSkeleton.load().then((tracer) => {
@@ -37,15 +51,6 @@ class Component extends HTMLElement {
         this.subscriptions.forEach((cb) => cb({ polylines: result.polylines, svg: vizResult }))
       })
     })
-  }
-  send({ imageData, preview }) {
-    this.property.preview.value = !!preview
-    this.render({ imageData })
-  }
-  subscriptions = []
-  subscribe(cb) {
-    this.subscriptions.push(cb)
-    return () => this.subscriptions.unshift(cb)
   }
 }
 customElements.define(Component.tag, Component)
